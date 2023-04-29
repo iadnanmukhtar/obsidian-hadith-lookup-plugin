4422c879f328c0665b6a9b73a46f56d9ab690691
jarfile <adnanmukhtar@gmail.com>
2023-04-29 19:36:30 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # obsidian-hadith-lookup-plugin
-An Obsidian plugin that uses selected text as a reference ID to lookup and insert Quran and Hadith text and translation using the Ḥadīth Unlocked API (lhttps://hadith.quranunlocked.com)
+
+An Obsidian plugin that uses selected text as a reference ID to lookup and insert Quran and Hadith text and translation using the **[Ḥadīth Unlocked](https://hadith.quranunlocked.com)** API.
+
+## Usage
+- Within Obsdian, find and install the plugin from the Community plugins
+- Within your Obsidian Note, type in a Ḥadīth Unlocked reference ID, e.g. quran:2:255 or bukhari:99, ideally on a new line
+- Select the reference text you typed-in ealier
+- Open the Obsidian Command palette and search for "Fetch Hadith"
+- The reference text will be replaced by the content of that reference from Ḥadīth Unlocked
+
+## References
+There are two types of references you can use with this plugin:
+### Quran: Single Āyah or a Range
+- quran:2:255
+- quran:3:190-194
+- etc.
+### Hadith
+- bukhari:99
+- muslim:1
+- etc.
+### Hadith Book IDs
+The Book IDs you can use for Hadith references are the ones available on Ḥadīth Unlocked. There are [several books available](https://hadith.quranunlocked.com/books). Browser the aḥādīth within the books and use the ID found in the browser URL. The ones currently available are: 
+- The two authentics: _bukhari_, _muslim_
+- The four sunans: _nasai_, _abudawud_, _tirmidhi_, _ibnmajah_
+- The additional: _ahmad_, _darami_, _hakim_, _ibnhibban_, _tabarani_, _nasai-kubra_, _bayhaqi_
+- and the comprehensive: _suyuti_
+
+### Customization of replacement blocks
+By default, the Obsidian Callout format is used to replace the reference with the contents from the Ḥadīth Unlocked API. The API returns a JSON array as a response and the first element of the array is the referenced hadith.
+
+The default Callout templates found in the plugin Settings can be customized as needed. The template should contain the JavaScript object-notation surrounded by {} to access different values from the response JSON. 
+
+For example, `{result[0].body_en}` in the Settings template would be replaced by the contents of the following JSON:
+```json
+[ 
+  { 
+	"body_en": "Some value"
+  } 
+]
+```
+See the example JSON respones of Ḥadīth Unlocked for the following references:
+- [quran:2:255](https://hadith.quranunlocked.com/quran:2:255?json)
+- [quran:3:190-194](https://hadith.quranunlocked.com/passage:3:190-194?json)
+- [bukhari:99](https://hadith.quranunlocked.com/bukhari:99?json)
+
+### Default replacement templates
+- **Quran (Single Āyah)**
+```
+> [!note]
+> {result[0].chapter.title} {result[0].num_ar} - {result[0].body}
+> 
+> [[{result[0].chapter.title_en} {result[0].num}]] - {result[0].body_en}
+```
+- **Quran (Āyah Range / Passage)**
+```
+> [!note]
+> {result[0].chapter.title} {result[0].num_ar} - {result[0].body}
+> 
+> [[{result[0].chapter.title_en} {result[0].num}]] - {result[0].body_en}
+```
+- **Hadith**
+```
+> [!tip] {result[0].title_en}
+> {result[0].book.shortName} {result[0].num} - {result[0].chain}
+> {result[0].body} 
+> {result[0].footnote} – {result[0].grade.grade} ({result[0].grader. shortName})
+> 
+> [[{result[0].book.shortName_en} {result[0].num}]] - {result[0].chain_en}
+> {result[0].body_en}
+> {result[0].footnote_en} – {result[0].grade.grade_en} ({result[0].grader. shortName_en})
+```
+
---
+++ main.ts
@@ -0,0 +1,180 @@
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface HadithLookupSettings {
+	api: string;
+	quranTemplate: string;
+	passageTemplate: string;
+	hadithTemplate: string;
+}
+
+const DEFAULT_SETTINGS: HadithLookupSettings = {
+	api: 'https://hadith.quranunlocked.com/{result.ref}?json',
+	quranTemplate:
+`> [!note]
+> {result[0].chapter.title} {result[0].num_ar} - {result[0].body}
+> 
+> [[{result[0].chapter.title_en} {result[0].num}]] - {result[0].body_en}
+
+
+`,
+	passageTemplate:
+`> [!note]
+> {result[0].chapter.title} {result[0].num_ar} - {result[0].body}
+> 
+> [[{result[0].chapter.title_en} {result[0].num}]] - {result[0].body_en}
+
+`,
+	hadithTemplate:
+`> [!tip] {result[0].title_en}
+> {result[0].book.shortName} {result[0].num} - {result[0].chain}
+> {result[0].body} 
+> {result[0].footnote} – {result[0].grade.grade} ({result[0].grader. shortName})
+> 
+> [[{result[0].book.shortName_en} {result[0].num}]] - {result[0].chain_en}
+> {result[0].body_en}
+> {result[0].footnote_en} – {result[0].grade.grade_en} ({result[0].grader. shortName_en})
+
+`,
+}
+
+export default class HadithLookupPlugin extends Plugin {
+
+	settings: HadithLookupSettings;
+
+	async onload() {
+
+		await this.loadSettings();
+
+		const ribbonIconEl = this.addRibbonIcon('book', 'Hadith Lookup', (evt: MouseEvent) => {
+		});
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		this.addCommand({
+			id: 'fetch-hadith-command',
+			name: 'Fetch Hadith',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				let ref = editor.getSelection();
+				let templateType = 'hadith';
+				if (ref.startsWith('quran'))
+					templateType = 'quran';
+				if (ref.match(/^quran:\d+:\d+-\d+/)) {
+					ref = ref.replace(/^quran/, 'passage');
+					templateType = 'passage';
+				}
+				console.log(ref);
+				try {
+					const res = await fetch(fillIn(this.settings.api, { ref: ref }));
+					const resStr = await res.text();
+					const result = (resStr === '') ? {} : JSON.parse(resStr);
+					result[0].num = (result[0].num + '').replace(/:/, '\ua789');
+					
+					if (templateType === 'hadith')
+						editor.replaceSelection(fillIn(this.settings.hadithTemplate, result));
+					else if (templateType === 'quran')
+						editor.replaceSelection(fillIn(this.settings.quranTemplate, result));
+					else if (templateType === 'passage')
+						editor.replaceSelection(fillIn(this.settings.passageTemplate, result));
+					else
+						editor.replaceSelection(JSON.stringify(result[0]));
+					
+				} catch (error) {
+					new Notice(`Lookup Failed: ${error.message}`);
+					console.error(error.stack);
+				}
+
+			}
+		});
+
+		this.addSettingTab(new HadithLookupSettingTab(this.app, this));
+
+	}
+
+	onunload() {
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class HadithLookupSettingTab extends PluginSettingTab {
+
+	plugin: HadithLookupPlugin;
+
+	constructor(app: App, plugin: HadithLookupPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', { text: 'Hadith Lookup Plugin Settings' });
+
+		new Setting(containerEl)
+			.setName('API')
+			.setDesc('Hadith Lookup API: Default is the Ḥadīth Unlocked API')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.api)
+				.onChange(async (value) => {
+					this.plugin.settings.api = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Quran Template')
+			.setDesc('For single ayah references, e.g. quran:2:255')
+			.addTextArea(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.quranTemplate)
+				.onChange(async (value) => {
+					this.plugin.settings.quranTemplate = value;
+					await this.plugin.saveSettings();
+				}));
+
+
+		new Setting(containerEl)
+			.setName('Quran Passage Template')
+			.setDesc('For passage references e.g. quran:2:255-258')
+			.addTextArea(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.passageTemplate)
+				.onChange(async (value) => {
+					this.plugin.settings.passageTemplate = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Hadith Template')
+			.setDesc('For hadith references e.g. muslim:55a of Hadith Unlocked')
+			.addTextArea(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.hadithTemplate)
+				.onChange(async (value) => {
+					this.plugin.settings.hadithTemplate = value;
+					await this.plugin.saveSettings();
+				}));
+
+	}
+}
+
+function fillIn(s: string, result: any) {
+	const matches = [...s.matchAll(/{([^}]+)}/g)];
+	if (matches) {
+		for (let i = 0; i < matches.length; i++) {
+			let replacement = eval(matches[i][1]);
+			if (!replacement) replacement = '';
+			s = s.replaceAll(matches[i][0], replacement);
+		}
+	}
+	return s;
+}
+
